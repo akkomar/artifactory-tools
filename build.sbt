d029caf5f3956f38be3770bf0f2beefd983b2601
akkomar <akkomar@users.noreply.github.com>
2015-07-28 14:11:53 +0200

    build & main

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "artifactory"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "1.1.5"
+libraryDependencies += "com.typesafe.play" %% "play-json" % "2.3.4"
+libraryDependencies += "com.typesafe" % "config" % "1.3.0"
+    
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,34 @@
+import play.api.libs.json
+import play.api.libs.json.Json
+import com.typesafe.config.ConfigFactory
+
+object Main extends App {
+
+  import java.time.{ZoneOffset, LocalDateTime, LocalDate}
+  import scalaj.http._
+  val date = LocalDate.of(2014,2,28).atStartOfDay().toEpochSecond(ZoneOffset.ofTotalSeconds(0)) * 1000
+
+  val conf = ConfigFactory.load()
+
+
+  private val artifactoryUrl: String = conf.getString("artifactory.apiUrl")
+
+
+  private val apiUser: String = conf.getString("artifactory.apiUser")
+  private val apiPassword: String = conf.getString("artifactory.apiPassword")
+  val response: HttpResponse[String] = Http(s"$artifactoryUrl/api/search/creation?from=1&to=$date&repos=ext-snapshot-local").option(HttpOptions.readTimeout(5000)).auth(apiUser,apiPassword).asString
+//  response.body
+//  response.code
+//  response.headers
+
+  val artifactUrls = (Json.parse(response.body) \\ "uri").map(_.as[String])
+
+//  println(artifactUrls)
+  println(artifactUrls.size)
+
+  artifactUrls.foreach{artifactUrl=>
+    val artifact = artifactUrl.split("storage").last
+    val res = Http(s"$artifactoryUrl/$artifact").option(HttpOptions.readTimeout(5000)).method("DELETE").auth(apiUser,apiPassword).asString
+    println(res)
+  }
+}
